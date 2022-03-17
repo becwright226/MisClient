7f12cc66fdbfe60f9469ac413ba8b1f79c87530b
becwright226 <becwright226@gmail.com>
2022-03-17 11:22:55 -0400

    switched localhost back to proper port in environment file

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,7 +4,7 @@ switch (window.location.hostname) {
     //
     case 'localhost' || '127.0.0.1':
         //
-    APIURL = 'http://localhost:2260';
+    APIURL = 'http://localhost:2206';
     break;
 
     case 'misenplas-gpir.herokuapp.com':
